caa0185a34b615f73fafc43777277f1d879f29ca
Sagleft <sagleft@inbox.ru>
2020-07-02 22:10:12 +0300

    update lib code

--- lib/utopialib.rb
+++ lib/utopialib.rb
@@ -1,13 +1,13 @@
-require 'net/http'
-require 'json'
+require "net/http"
+require "json"
 
 module UtopiaRuby
   class Client
-    @protocol = "http"
+    @@protocol = "http"
+    @@api_v = "1.0"
     @host  = "127.0.0.1"
     @token = ""
     @port  = 22820
-    @api_v = "1.0"
 
     def initialize(host = "127.0.0.1", token = "", port = 22820)
       @host  = host
@@ -15,22 +15,43 @@ module UtopiaRuby
       @port  = port
     end
   
-    def self.ApiQuery(query_method = "GetSystemInfo")
-      query_array = {method: query_method, token: @token, params: nil}
-      query_json  = query_array.to_json
-      endpoint = @protocol + "://" + @host + ":" + port.to_s + "/api/" + @api_v + "/"
-
-      http = Net::HTTP.new(uri.host, uri.port)
-      request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
-      request.body = query_json
-      result = http.request(req)
-  
-      #json response
-      return result.body
+    def SetClientProtocol(protocol = "http")
+      @@protocol = protocol
+    end
+    
+    def SetClientAPIVersion(api_v = "1.0")
+      @@api_v = api_v
     end
   
+    def ApiQuery(query_method = "GetSystemInfo")
+      query_array = {method: query_method, token: @token, params: nil}
+      
+      #puts @host #debug
+      #return "" #debug
+      endpoint = @@protocol + "://" + @host + ":" + @port.to_s + "/api/" + @@api_v + "/"
+      #puts endpoint #debug
+
+      begin
+        uri = URI.parse(endpoint)
+        header = {"Content-Type": "text/json"}
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Post.new(uri.request_uri, header)
+        request.body = query_array.to_json
+        response = http.request(request)
+      rescue SocketError
+        #puts "SocketError"
+        return ""
+      end
+
+      return response.body
+    end
+
     def GetSystemInfo
-      return JSON.parse(ApiQuery())
+      result = ApiQuery()
+      if result == ""
+        return {}
+      end
+      return JSON.parse(result)
     end
   end
 end
