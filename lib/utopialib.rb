035157bd4dde1d24c5de32c5ee1b243cd2a56099
Sagleft <sagleft@inbox.ru>
2020-07-09 14:48:21 +0300

    update lib

--- lib/utopialib.rb
+++ lib/utopialib.rb
@@ -1,5 +1,6 @@
 require "net/http"
 require "json"
+require 'boolean'
 
 module UtopiaRuby
   class Client
@@ -15,16 +16,16 @@ module UtopiaRuby
       @port  = port
     end
   
-    def SetClientProtocol(protocol = "http")
+    def setClientProtocol(protocol = "http")
       @@protocol = protocol
     end
     
-    def SetClientAPIVersion(api_v = "1.0")
+    def setClientAPIVersion(api_v = "1.0")
       @@api_v = api_v
     end
   
-    def ApiQuery(query_method = "GetSystemInfo")
-      query_array = {method: query_method, token: @token, params: nil}
+    def apiQuery(query_method = "getSystemInfo", qparams = {})
+      query_array = {method: query_method, token: @token, params: qparams}
       
       #puts @host #debug
       #return "" #debug
@@ -41,17 +42,50 @@ module UtopiaRuby
       rescue SocketError
         #puts "SocketError"
         return ""
+      rescue Errno::ECONNREFUSED
+        return ""
       end
 
       return response.body
     end
 
-    def GetSystemInfo
-      result = ApiQuery()
+    def getSystemInfo
+      return parseResult(apiQuery())
+    end
+    
+    def parseResult(result = {}, byDefault = {})
       if result == ""
-        return {}
+        return byDefault
       end
-      return JSON.parse(result)
+      parsed = JSON.parse(result)
+      if parsed.include? "result" == false
+        return byDefault
+      end
+      return parsed["result"]
+    end
+    
+    def connected?
+      if getSystemInfo() == {}
+        return false
+      else
+        return true
+      end
+    end
+    
+    def getProfileStatus
+      return parseResult(apiQuery("getProfileStatus"))
+    end
+    
+    def setProfileStatus(status = "Available", mood = "")
+      params = {status: status, mood: mood}
+      result = parseResult(
+        apiQuery("setProfileStatus", params),
+        false
+      )
+      if result.is_a?(Boolean) == false
+        return false
+      end
+      return true
     end
   end
 end
--- utopialib.gemspec
+++ utopialib.gemspec
@@ -1,13 +1,14 @@
 Gem::Specification.new do |s|
-  s.name        = "utopialib"
-  s.version     = "1.0.0"
-  s.date        = "2020-06-24"
-  s.summary     = "UtopiaLib"
-  s.description = "Ruby on Rails library for Utopia Network API"
-  s.authors     = ["Sagleft"]
-  s.email       = "saget@mfc.click"
-  s.files       = ["lib/utopialib.rb"]
+  s.name          = "utopialib"
+  s.version       = "1.0.0"
+  s.date          = "2020-06-24"
+  s.summary       = "UtopiaLib"
+  s.description   = "Ruby library for Utopia Network API"
+  s.authors       = ["Sagleft"]
+  s.email         = "saget@mfc.click"
+  s.files         = ["lib/utopialib.rb"]
   s.require_paths = ["lib"]
-  s.homepage    = "https://u.is/"
+  s.homepage      = "https://u.is/"
   s.license       = "MIT"
+  s.add_dependency 'boolean', '~> 1.0'
 end
