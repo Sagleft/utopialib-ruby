b03aad0f5b6a100fe87d59d0e4a0db493893cbad
Sagleft <sagleft@inbox.ru>
2020-07-09 16:29:44 +0300

    add new methods

--- lib/utopialib.rb
+++ lib/utopialib.rb
@@ -3,6 +3,18 @@ require "json"
 require 'boolean'
 
 module UtopiaRuby
+  class QueryFilter
+    @@sortBy = ""
+    @@offset = ""
+    @@limit  = ""
+    
+    def initialize(sortBy = "", offset = "", limit = "")
+      @@sortBy = sortBy
+      @@offset = offset
+      @@limit  = limit
+    end
+  end
+
   class Client
     @@protocol = "http"
     @@api_v = "1.0"
@@ -24,8 +36,11 @@ module UtopiaRuby
       @@api_v = api_v
     end
   
-    def apiQuery(query_method = "getSystemInfo", qparams = {})
+    def apiQuery(query_method = "getSystemInfo", qparams = {}, query_filter = nil)
       query_array = {method: query_method, token: @token, params: qparams}
+      if query_filter != nil
+        query_array["filter"] = query_filter
+      end
       
       #puts @host #debug
       #return "" #debug
@@ -64,6 +79,20 @@ module UtopiaRuby
       return parsed["result"]
     end
     
+    def parseBool(value = false)
+      if value.is_a?(Boolean) == false
+        return false
+      end
+      return true
+    end
+    
+    def parseInt(value)
+      if !(value >= 0)
+        return 0
+      end
+      return value
+    end
+    
     def connected?
       if getSystemInfo() == {}
         return false
@@ -82,10 +111,128 @@ module UtopiaRuby
         apiQuery("setProfileStatus", params),
         false
       )
-      if result.is_a?(Boolean) == false
-        return false
-      end
-      return true
+      return parseBool(result)
     end
+    
+    def getOwnContact
+      return parseResult(apiQuery("getOwnContact"))
+    end
+    
+    def getContacts(searchFilter = "", query_filter = nil)
+      params = {filter: searchFilter}
+      return parseResult(apiQuery("getContacts", params, query_filter))
+    end
+    
+    def getContactAvatar(pk = "", coder = "BASE64", format = "PNG")
+      params = {
+        pk: pk,
+        coder: coder,
+        format: format
+      }
+      return parseResult(apiQuery("getContactAvatar", params), "")
+    end
+    
+    def getChannelAvatar(channelid = "", coder = "BASE64", format = "PNG")
+      params = {
+        channelid: channelid,
+        coder: coder,
+        format: format
+      }
+      return parseResult(apiQuery("getChannelAvatar", params), "")
+    end
+    
+    def setContactGroup(pk = "", groupName = "group")
+      params = {contactPublicKey: pk, groupName: groupName}
+      result = parseResult(
+        apiQuery("setContactGroup", params),
+        false
+      )
+      return parseBool(result)
+    end
+    
+    def setContactNick(pk = "", newNick = "user")
+      params = {contactPublicKey: pk, newNick: newNick}
+      result = parseResult(
+        apiQuery("setContactNick", params),
+        false
+      )
+      return parseBool(result)
+    end
+    
+    def sendInstantMessage(pkOrNick = "", message = "test message")
+      params = {to: pkOrNick, text: message}
+      result = parseResult(
+        apiQuery("sendInstantMessage", params),
+        0
+      )
+      return parseInt(result)
+    end
+    
+    def sendInstantQuote(pkOrNick = "", text = "instant quoute", id_message = 100)
+      params = {
+        to: pkOrNick,
+        text: text,
+        id_message: id_message
+      }
+      result = parseResult(
+        apiQuery("sendInstantQuote", params),
+        0
+      )
+      return parseInt(result)
+    end
+    
+    def sendInstantSticker(pkOrNick = "", collection = "434", name = "343")
+      params = {to: pkOrNick, collection: collection, name: name}
+      result = parseResult(
+        apiQuery("sendInstantSticker", params),
+        0
+      )
+      return parseInt(result)
+    end
+    
+    def getStickerCollections
+      return parseResult(apiQuery("getStickerCollections"))
+    end
+    
+    def getImageSticker(collection = "Default Stickers", stickerName = "airship", coder = "BASE64")
+      params = {collection_name: collection, sticker_name: stickerName, coder: coder}
+      return parseResult(apiQuery("getImageSticker", params), "")
+    end
+    
+    def sendInstantBuzz(pkOrNick = "", comments = "test")
+      params = {to: pkOrNick, comments: comments}
+      result = parseResult(
+        apiQuery("sendInstantBuzz", params),
+        0
+      )
+      return parseInt(result)
+    end
+    
+    def sendInstantInvitation(pkOrNick = "", channelid = "", description = "", comments = "")
+      params = {to: pkOrNick, channelid: channelid, description: description, comments: comments}
+      result = parseResult(
+        apiQuery("sendInstantInvitation", params),
+        0
+      )
+      return parseInt(result)
+    end
+    
+    def removeInstantMessages(pk = "")
+      params = {hex_contact_public_key: pk}
+      result = parseResult(apiQuery("removeInstantMessages", params), false)
+      return parseBool(result)
+    end
+    
+    def getContactMessages(pk = "", query_filter = nil)
+      params = {pk: pk}
+      return parseResult(apiQuery("getContactMessages", params, query_filter))
+    end
+    
+    def sendEmailMessage(pkOrNick = "", subject = "test message", body = "message content")
+      params = {to: [pkOrNick], subject: subject, body: body}
+      result = parseResult(apiQuery("sendEmailMessage", params))
+      return parseBool(result)
+    end
+    
   end
 end
